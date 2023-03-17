701d767fccdcf12184836b94a51baf4d5f1e8159
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-03-17 10:46:20 +0530

    added push notifications page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import { BsChevronDown } from "react-icons/bs";
 import * as PushAPI from "@pushprotocol/restapi";
 
-// import * as PushAPI from "@pushprotocol/restapi";
-
 import Image from "next/image";
 import rarxlogo from "../../public/rarxpng.png";
 import filecoinLogo from "../../public/chains/filecoin.png";
@@ -340,7 +338,6 @@ const Navbar = ({ connectToWallet, signer, signer_address, signer_bal, connectTo
   };
 
   const getNotifications = () => {
-    // console.log({ signer: signer_address })
     PushAPI.user
       .getFeeds({
         user: `eip155:${chainIdMain}:${signer_address}`,
@@ -1095,6 +1092,25 @@ const Navbar = ({ connectToWallet, signer, signer_address, signer_bal, connectTo
                         </svg>
                         <span className="mt-1 font-display text-sm text-jacarta-700 dark:text-white">
                           Edit Profile
+                        </span>
+                      </Link>
+                      <Link
+                        href="/Notifications"
+                        className="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-accent focus:text-accent dark:hover:bg-jacarta-600"
+                      >
+                        <svg
+                          className="h-5 w-5 fill-jacarta-700"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+
+                          />
+                        </svg>
+                        <span className="mt-1 font-display text-sm text-jacarta-700 dark:text-white">
+                          Notifications
                         </span>
                       </Link>
                       <a
---
+++ src/pages/Notifications.jsx
@@ -0,0 +1,207 @@
+import { React, useState, useEffect } from "react";
+import * as PushAPI from "@pushprotocol/restapi";
+import Image from "next/image";
+
+
+
+const Notifications = ({ connectToWallet, signer_address, chainIdMain }) => {
+
+    const [notificationData, setNotificationData] = useState();
+    const [propShow, setPropShow] = useState(true);
+
+
+    const getNotifications = () => {
+        PushAPI.user
+            .getFeeds({
+                user: `eip155:${chainIdMain}:${signer_address}`,
+                env: "staging",
+                page: 1,
+                limit: 10,
+            })
+            .then((feeds) => {
+                setNotificationData(feeds);
+                console.log(feeds)
+            })
+            .catch((err) => {
+                console.error("failed to get user notifications: ", err);
+            });
+    };
+
+    useEffect(() => {
+        connectToWallet();
+        getNotifications();
+    }, [chainIdMain, signer_address]);
+
+    return (
+        // < !--Activity -- >
+        <section class="relative py-24">
+            <picture class="pointer-events-none absolute inset-0 -z-10 dark:hidden">
+                <img src="img/gradient_light.jpg" alt="gradient" class="h-full w-full" />
+            </picture>
+            <div class="container">
+                <h1 class="mt-16 text-center font-display text-4xl font-medium text-jacarta-700 dark:text-white">Notifications</h1>
+                <p className="mb-16 mt-2 text-[18px] text-center leading-normal dark:text-jacarta-300">
+                    All the notifications are realtime and powered by push protocol
+                </p>
+
+                <div className="scrollbar-custom overflow-x-auto rounded-lg">
+                    <div className="min-w-fit">
+                        <ul className="nav nav-tabs flex items-center" role="tablist">
+                            <li
+                                className="nav-item"
+                                role="presentation"
+                                onClick={() => setPropShow(true)}
+                            >
+                                <button
+                                    className={`nav-link ${propShow &&
+                                        "active relative"} flex items-center whitespace-nowrap py-3 px-6 text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white`}
+                                    id="properties-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#properties"
+                                    type="button"
+                                    role="tab"
+                                    aria-controls="properties"
+                                    aria-selected="false"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="24"
+                                        height="24"
+                                        className="mr-1 h-5 w-5 fill-current"
+                                    >
+                                        <path fill="none" d="M0 0h24v24H0z" />
+                                        <path d="M6.17 18a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2v-2h4.17zm6-7a3.001 3.001 0 0 1 5.66 0H22v2h-4.17a3.001 3.001 0 0 1-5.66 0H2v-2h10.17zm-6-7a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2V4h4.17z" />
+                                    </svg>
+                                    <span className="font-display text-base font-medium">
+                                        Marketplace Notifications
+                                    </span>
+                                </button>
+                            </li>
+
+                            {/* <!-- Details --> */}
+                            <li
+                                className="nav-item"
+                                role="presentation"
+                                onClick={() => setPropShow(false)}
+                            >
+                                <button
+                                    className={`nav-link ${!propShow &&
+                                        "active relative"} flex items-center whitespace-nowrap py-3 px-6 text-jacarta-400 hover:text-jacarta-700 dark:hover:text-white`}
+                                    id="details-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#details"
+                                    type="button"
+                                    role="tab"
+                                    aria-controls="details"
+                                    aria-selected="false"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="24"
+                                        height="24"
+                                        className="mr-1 h-5 w-5 fill-current"
+                                    >
+                                        <path fill="none" d="M0 0h24v24H0z" />
+                                        <path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zM7 6h4v4H7V6zm0 6h10v2H7v-2zm0 4h10v2H7v-2zm6-9h4v2h-4V7z" />
+                                    </svg>
+                                    <span className="font-display text-base font-medium">
+                                        All Notification
+                                    </span>
+                                </button>
+                            </li>
+                        </ul>
+
+                        {/* <!-- Tab Contents --> */}
+                        <div className="tab-content">
+                            {propShow ? (
+                                <div>
+                                    <div className="rounded-t-2lg rounded-b-2lg rounded-tl-none border border-jacarta-100 bg-white p-6 dark:border-jacarta-600 dark:bg-jacarta-700 md:p-10">
+                                        <div class="mb-10 shrink-0 basis-8/12 space-y-5 lg:mb-0 lg:pr-10">
+                                            {notificationData?.map((e) => {
+                                                return (
+                                                    e.app === "RarX Marketplace" && (
+                                                        <div key={e.sid}>
+                                                            <a
+                                                                href={e.cta}
+                                                                class="relative flex items-center rounded-2.5xl border border-jacarta-100 bg-white p-8 transition-shadow hover:shadow-lg dark:border-jacarta-700 dark:bg-jacarta-700"
+                                                            >
+                                                                <div class="mr-5 self-start">
+                                                                    <Image src={e.icon} alt="avatar 2" class="rounded-2lg" loading="lazy" height={100} width={100} />
+                                                                </div>
+
+                                                                <div>
+                                                                    <h3 class="mb-1 font-display text-base font-semibold text-jacarta-700 dark:text-white">
+                                                                        {e.notification.title}
+                                                                    </h3>
+                                                                    <span class="mb-3 block text-sm text-jacarta-500 dark:text-jacarta-200">{e.notification.body}</span>
+                                                                    <span class="block text-xs text-jacarta-300">Via : {e.app} {" "} | {" "} Chain : {e.blockchain} </span>
+                                                                </div>
+
+                                                                <div class="ml-auto rounded-full font-bold border border-jacarta-100 p-3 dark:border-jacarta-600">
+                                                                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" /></svg>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    )
+                                                );
+                                            })}
+                                            {notificationData?.length == 0 && (
+                                                <h3 class="mb-1 text-[26px] text-center font-semibold text-jacarta-700 dark:text-white">
+                                                    No Notifications
+                                                </h3>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div>
+                                    <div className="rounded-t-2lg rounded-b-2lg rounded-tl-none border border-jacarta-100 bg-white p-6 dark:border-jacarta-600 dark:bg-jacarta-700 md:p-10">
+                                        <div class="mb-10 shrink-0 basis-8/12 space-y-5 lg:mb-0 lg:pr-10">
+                                            {notificationData?.map((e) => {
+                                                return (
+                                                    (
+                                                        <div key={e.sid}>
+                                                            <a
+                                                                href={e.cta}
+                                                                class="relative flex items-center rounded-2.5xl border border-jacarta-100 bg-white p-8 transition-shadow hover:shadow-lg dark:border-jacarta-700 dark:bg-jacarta-700"
+                                                            >
+                                                                <div class="mr-5 self-start">
+                                                                    <Image src={e.icon} alt="avatar 2" class="rounded-2lg" loading="lazy" height={100} width={100} />
+                                                                </div>
+
+                                                                <div>
+                                                                    <h3 class="mb-1 font-display text-base font-semibold text-jacarta-700 dark:text-white">
+                                                                        {e.notification.title}
+                                                                    </h3>
+                                                                    <span class="mb-3 block text-sm text-jacarta-500 dark:text-jacarta-200">{e.notification.body}</span>
+                                                                    <span class="block text-xs text-jacarta-300">Via : {e.app}</span>
+                                                                </div>
+
+                                                                <div class="ml-auto rounded-full font-bold border border-jacarta-100 p-3 dark:border-jacarta-600">
+                                                                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" /></svg>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    )
+                                                );
+                                            })}
+                                            {notificationData?.length == 0 && (
+                                                <h3 class="mb-1 text-[26px] text-center font-semibold text-jacarta-700 dark:text-white">
+                                                    No Notifications
+                                                </h3>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Notifications
